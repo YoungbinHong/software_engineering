dc190b872aa7a86e46fbffaf9e37892d8b5a38e6
Youngbin_Hong <code_archer@naver.com>
2020-09-16 12:51:11 +0900

    새로운 spring boot project 생성

---
+++ icehomepage/src/main/java/com/example/icehomepage/IcehomepageApplication.java
@@ -0,0 +1,13 @@
+package com.example.icehomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcehomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcehomepageApplication.class, args);
+	}
+
+}
---
+++ icehomepage/src/test/java/com/example/icehomepage/IcehomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icehomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcehomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
